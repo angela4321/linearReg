29f2e50da65ccd3db2ab8cad98454f53488121f8
angela4321 <angelaqiao621@gmail.com>
2020-07-13 19:33:45 -0700

    Debug regression code

--- Regression.py
+++ Regression.py
@@ -14,13 +14,14 @@ class Regression:
     def learn(self):
         for i in range(self.epochs):
            self.update()
+           print(str(self.cost()))
 
     def update(self):
         temp = []
         for i in range(len(self.theta)-1):
             sum=0
             for j in self.data:
-                p = self.predict(i)
+                p = self.predict(j)
                 sum+=(p-j[len(j)-1])*j[i]
 
             nTheta = self.theta[i]-sum/len(self.data)*self.a
@@ -29,12 +30,12 @@ class Regression:
         #update constant
         sum = 0
         for j in self.data:
-            p = self.predict(i)
+            p = self.predict(j)
             sum += (p - j[len(j) - 1])
-        nTheta = self.theta[i] - sum / len(self.data) * self.a
+        nTheta = self.theta[len(self.theta)-1] - sum / len(self.data) * self.a
         temp.append(nTheta)
 
-        theta = temp
+        self.theta = temp
 
 
     def predict(self,row):
@@ -44,3 +45,15 @@ class Regression:
         sum+=self.theta[len(self.theta)-1]
         return sum
 
+    def printTheta(self):
+        s = ""
+        for i in self.theta:
+            s+=str(i)+" "
+        print(s)
+
+    def cost(self):
+        sum = 0
+        for i in self.data:
+            temp = self.predict(i)-i[len(i)-1]
+            sum+=temp*temp
+        return sum
--- Test.py
+++ Test.py
@@ -1,7 +1,13 @@
 from GenerateFile import GenerateFile
+from Regression import Regression
 
-g = GenerateFile(1,1,5,1,5,4,10)
+g = GenerateFile(1,1,5,1,5,1,200)
 g.makeVariables()
 g.makeData()
 g.printVariables()
 g.printData()
+
+r = Regression(0.03,g.data,100)
+r.learn()
+g.printVariables()
+r.printTheta()
